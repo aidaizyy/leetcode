aaad9cadbf77b88ce5c984c3c6852631bd520c4c
Yunyao Zhang <aidaizyy@gmail.com>
2017-02-27 16:59:40 +0800

    add p100

---
+++ p100/p100/main.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  p100
+//
+//  Created by 张云尧 on 2017/2/27.
+//  Copyright © 2017年 张云尧. All rights reserved.
+//
+
+#include <iostream>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr)
+            return true;
+        if (p == nullptr || q == nullptr)
+            return false;
+        if (p->val != q->val)
+            return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    Solution s;
+    return 0;
+}
