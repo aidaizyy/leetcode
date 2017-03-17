53522705f3a0f4b30039e556d2893a5ecda46a6d
Yunyao Zhang <aidaizyy@gmail.com>
2017-03-17 16:05:00 +0800

    modify p287

--- p287/p287/main.cpp
+++ p287/p287/main.cpp
@@ -15,7 +15,7 @@ public:
     int findDuplicate(vector<int>& nums) {
         
         /*
-        //13ms
+        //13ms nlogn
         sort(nums.begin(), nums.end());
         for (int i = 0; i < nums.size() - 1; ++i) {
             if (nums[i] == nums[i+1])
@@ -24,22 +24,40 @@ public:
         return -1;
         */
         
-        //9ms
-        if (nums.size() > 1) {
-            int slow = nums[0];
-            int fast = nums[nums[0]];
-            while (slow != fast) {
-                slow = nums[slow];
-                fast = nums[nums[fast]];
+        /*
+        //9ms nlogn
+        int left = 1;
+        int right = nums.size() - 1;
+        while (left < right) {
+            int middle = left + (right - left) / 2;
+            int count = 0;
+            for (int num : nums) {
+                if (num <= middle)
+                    ++count;
             }
-            fast = 0;
-            while (slow != fast) {
-                slow = nums[slow];
-                fast = nums[fast];
-            }
-            return slow;
+            if (count > middle)
+                right = middle;
+            else
+                left = middle + 1;
         }
-        return -1;
+        return left;
+        */
+        
+        //9ms n
+        if (nums.size() <= 1)
+            return -1;
+        int slow = 0;
+        int fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        fast = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
     }
 };
 
